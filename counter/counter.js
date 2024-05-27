5ac6e82493bd5c9669f2f44ce5925f4aaee16842
Yamada-mashimashi <kuranpu_math@yahoo.co.jp>
2024-05-28 01:39:16 +0900

    カウンターサイトの改良

--- counter/counter.js
+++ counter/counter.js
@@ -1,16 +1,45 @@
 document.addEventListener("DOMContentLoaded", () => {
   const number = document.getElementById("h1");
+  const base = document.getElementById("base");
+  let slider = document.getElementById("slider");
+  const d = document.getElementById("d");
+  const minusButton = document.getElementById("dec");
   const plusButton = document.getElementById("inc");
   const resetButton = document.getElementById("reset");
 
   let count = 0;
+  let dx = parseInt(slider.value, 10);
+  const centerX = window.innerWidth / 2;
+  let x = centerX - number.offsetWidth / 2;
+
+  const baseRect = base.getBoundingClientRect();
+  let y = baseRect.top - baseRect.height - 10;
+
+  number.style.left = x + "px";
+  number.style.top = y + "px";
+
+  slider.addEventListener("input", () => {
+    dx = parseInt(slider.value);
+    d.innerHTML = dx;
+  });
+
+  minusButton.addEventListener("click", () => {
+    count--;
+    x -= dx;
+    number.innerHTML = count;
+    number.style.left = x + "px";
+  });
 
   plusButton.addEventListener("click", () => {
     count++;
+    x += dx;
     number.innerHTML = count;
+    number.style.left = x + "px";
   });
   resetButton.addEventListener("click", () => {
     count = 0;
     number.innerHTML = 0;
+    x = centerX - number.offsetWidth / 2;
+    number.style.left = x + "px";
   });
 });
